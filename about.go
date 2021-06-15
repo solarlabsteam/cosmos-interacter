6071f84caf36180052c02e1b6af70f7adf0f3d0c
Techno Freak <freak12techno@gmail.com>
2021-06-16 00:21:53 +0300

    chore: fix formatting

--- about.go
+++ about.go
@@ -10,9 +10,9 @@ func getAbout(message *tb.Message) {
 	var sb strings.Builder
 	sb.WriteString("<strong>cosmos-interacter</strong>\n\n")
 	sb.WriteString("A bot that can return info about Cosmos-based blockchain params. Check /help for more info.\n")
-	sb.WriteString("Created by<a href=\"https://validator.solar\">SOLAR Labs</a> with ❤️.\n")
+	sb.WriteString("Created by <a href=\"https://validator.solar\">SOLAR Labs</a> with ❤️.\n")
 	sb.WriteString("This bot is open-sourced, you can get the source code at https://github.com/solarlabsteam/cosmos-interacter.\n\n")
-	sb.WriteString("We also maintain the following tools for Cosmos ecosystem:")
+	sb.WriteString("We also maintain the following tools for Cosmos ecosystem:\n")
 	sb.WriteString("- <a href=\"https://github.com/solarlabsteam/missed-blocks-checker\">missed-blocks-checker</a> - monitor for validator' missing blocks\n")
 	sb.WriteString("- <a href=\"https://github.com/solarlabsteam/cosmos-exporter\">cosmos-exporter</a> - scrape the blockchain data from the local node and export it to Prometheus\n")
 	sb.WriteString("- <a href=\"https://github.com/solarlabsteam/coingecko-exporter\">coingecko-exporter</a> - scrape the Coingecko exchange rate and export it to Prometheus\n")
