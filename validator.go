75af0a6ab158e38e97ff43d88225be030bdec14e
Techno Freak <freak12techno@gmail.com>
2021-06-15 15:46:17 +0300

    chore: fixed validator formatting, add rank

--- validator.go
+++ validator.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"sort"
+	"strconv"
 	"strings"
 
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
@@ -27,17 +29,54 @@ func getValidatorInfo(message *tb.Message) {
 		return
 	}
 
+	rank, err := getValidatorRank(validator)
+	if err != nil {
+		log.Error().Err(err).Msg("Could not get validator rank")
+		return
+	}
+
 	// --------------------------------
 
 	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("<code>%s</code>\n", validator.Description.Moniker))
 	sb.WriteString(fmt.Sprintf("<a href=\"https://mintscan.io/%s/validators/%s\">Mintscan</a>\n\n", MintscanPrefix, validator.OperatorAddress))
 
-	sb.WriteString(fmt.Sprintf("<strong>Moniker: </strong>%s\n", validator.Description.Moniker))
-	sb.WriteString(fmt.Sprintf("<strong>Operator address: </strong>%s\n", validator.OperatorAddress))
-	sb.WriteString(fmt.Sprintf("<strong>Description: </strong>%s\n", validator.Description.Details))
-	sb.WriteString(fmt.Sprintf("<strong>Website: </strong>%s\n", validator.Description.Website))
-	sb.WriteString(fmt.Sprintf("<strong>Security contact: </strong>%s\n", validator.Description.SecurityContact))
+	sb.WriteString(fmt.Sprintf("<strong>Moniker: </strong><code>%s</code>\n", validator.Description.Moniker))
+	sb.WriteString(fmt.Sprintf("<strong>Operator address: </strong><code>%s</code>\n", validator.OperatorAddress))
+	sb.WriteString(fmt.Sprintf("<strong>Description: </strong><code>%s</code>\n", validator.Description.Details))
+	sb.WriteString(fmt.Sprintf("<strong>Website: </strong><code>%s</code>\n", validator.Description.Website))
+	sb.WriteString(fmt.Sprintf("<strong>Security contact: </strong><code>%s</code>\n", validator.Description.SecurityContact))
+
+	// because cosmos's dec doesn't have .toFloat64() method or whatever and returns everything as int
+	if value, err := strconv.ParseFloat(validator.Commission.CommissionRates.Rate.String(), 64); err != nil {
+		log.Error().
+			Str("address", address).
+			Err(err).
+			Msg("Could not parse balance")
+		return
+	} else {
+		sb.WriteString(fmt.Sprintf("<strong>Commission rate: </strong><code>%.1f%%</code>\n", value*100))
+	}
+
+	if value, err := strconv.ParseFloat(validator.DelegatorShares.String(), 64); err != nil {
+		log.Error().
+			Str("address", address).
+			Err(err).
+			Msg("Could not parse balance")
+		return
+	} else {
+		sb.WriteString(fmt.Sprintf(
+			"\n<strong>Total tokens delegated: </strong><code>%.1f %s</code>\n",
+			value/DenomCoefficient,
+			Denom,
+		))
+	}
+
+	if validator.Jailed {
+		sb.WriteString("<strong>Rank: </strong>JAILED\n")
+	} else {
+		sb.WriteString(fmt.Sprintf("<strong>Rank: </strong>%d\n", rank))
+	}
 
 	bot.Send(
 		message.Chat,
@@ -95,5 +134,35 @@ func getValidator(address string) (stakingtypes.Validator, error) {
 		}
 	}
 
-	return stakingtypes.Validator{}, fmt.Errorf("Validator is not found")
+	return stakingtypes.Validator{}, fmt.Errorf("validator is not found")
+}
+
+func getValidatorRank(validator stakingtypes.Validator) (int, error) {
+	stakingClient := stakingtypes.NewQueryClient(grpcConn)
+	validatorsResponse, err := stakingClient.Validators(
+		context.Background(),
+		&stakingtypes.QueryValidatorsRequest{},
+	)
+
+	if err != nil {
+		log.Error().
+			Str("address", validator.OperatorAddress).
+			Err(err).
+			Msg("Could not get validators")
+		return 0, err
+	}
+
+	validators := validatorsResponse.Validators
+
+	sort.Slice(validators[:], func(i, j int) bool {
+		return validators[i].DelegatorShares.RoundInt64() > validators[j].DelegatorShares.RoundInt64()
+	})
+
+	for index, iteratedValidator := range validators {
+		if validator.OperatorAddress == iteratedValidator.OperatorAddress {
+			return index + 1, nil
+		}
+	}
+
+	return 0, fmt.Errorf("could not find validator rank")
 }
